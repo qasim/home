aba9973bcbbd30b161a4f83b4283a114669262f3
Qasim Iqbal <qasim.iqbal@instacart.com>
2022-01-25 19:19:32 -0500

    Add REAPER

---
+++ reaper/configure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+
+echo "Configuring microphone"
+mkdir -p "~/Documents/REAPER Media"
+cp -rf $DIR/Microphone.rpp "~/Documents/REAPER Media/Microphone.rpp"
+cp -rf $DIR/Microphone.rpp "~/Documents/REAPER Media/Microphone.RfxChain"
+
+# Create an aggregate device in Audio MIDI Setup including
+# "Blackhole 2ch" and "Scarlett 2i2 USB", and name it REAPER
