e1c9a87f542468b076624480301dbe52bd5947a6
Qasim Iqbal <qasim.iqbal@instacart.com>
2022-01-30 10:59:56 -0500

    Update .gitconfig-instacart

--- .home/brew/setup.sh
+++ .home/brew/setup.sh
@@ -5,7 +5,6 @@ if ! [ -x "$(which brew)" ]; then
   echo "Installing Homebrew"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-brew tap homebrew/cask-versions
 
 echo "Running Brewfile"
 brew bundle --file=~/.home/brew/Brewfile
