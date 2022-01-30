088bd0a8fd70d74657a3ec9a935c75b682f3de55
Qasim Iqbal <me@qas.im>
2022-01-29 22:59:00 -0500

    Only block private submodule setup if submodule isn't cloned

--- .home/private/setup.sh
+++ .home/private/setup.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-echo "To setup the private submodule, you may need to:"
-echo
-echo "1. Open 1Password.app and log in"
-echo "2. Visit https://github.com/settings/tokens/new and generate a new token"
-echo "3. Paste the token as your GitHub password if prompted"
-echo
-read -p "Ready to setup the private submodule? " -r
-echo
-if [[ $REPLY =~ ^[Yy]$ || -z $REPLY ]]; then
+setup_private_submodule () {
     echo "Fetching private submodule"
     git submodule update --init
 
     echo "Setting up private submodule"
     ~/.private/setup.sh
+}
+
+if [ ! -f ~/.private/setup.sh ]; then
+    echo "To setup the private submodule, you may need to:"
+    echo
+    echo "1. Open 1Password.app and log in"
+    echo "2. Visit https://github.com/settings/tokens/new and generate a new token"
+    echo "3. Paste the token as your GitHub password if prompted"
+    echo
+    read -p "Ready to setup the private submodule? " -r
+    echo
+    setup_private_submodule
 else
-    echo "Skipping private submodule setup"
+    setup_private_submodule
 fi
