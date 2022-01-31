f8be8246036da173341ac3e8835b4ade0ef10a34
Qasim Iqbal <qasim.iqbal@instacart.com>
2022-01-31 11:53:13 -0500

    Update homebrew install to bash

--- .home/brew/setup.sh
+++ .home/brew/setup.sh
@@ -3,7 +3,7 @@
 echo "Setting up Homebrew"
 if ! [ -x "$(which brew)" ]; then
   echo "Installing Homebrew"
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 echo "Running Brewfile"
