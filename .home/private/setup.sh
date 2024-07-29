e38b743efdb2883441178f2a96a43fb320bfa6ec
Qasim Iqbal <me@qas.im>
2024-07-29 13:39:28 -0400

    make private happen as its own repo

--- .home/private/setup.sh
+++ .home/private/setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# TODO: make this happen without a submodule
+echo "Fetching private configuration"
+cd $HOME && git clone https://github.com/qasim/private.git .private
 
-echo "Fetching private submodule"
-cd $HOME && git submodule update --init .private
-
-echo "Setting up private submodule"
+echo "Setting up private configuration"
 ~/.private/setup.sh
 
-echo "Cleaning up private submodule"
+echo "Cleaning up private configuration"
 chmod -R a+w ~/.private
 rm -r ~/.private
